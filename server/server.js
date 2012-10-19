ea473b60cef785451ce52c6345edee82a2426566
Lwin Maung <lwin@maungs.com>
2012-10-18 22:40:02 -0500

    websocket test

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 var http = require('http')
 var port = process.env.port || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var ws = require('websocket.io')
+    , http = require('http').createServer().listen(port)
+    , server = ws.attach(http)
+
+server.on('connection', function (socket) {
+    socket.on('message', function () { });
+    socket.on('close', function () { });
+});
+
+
